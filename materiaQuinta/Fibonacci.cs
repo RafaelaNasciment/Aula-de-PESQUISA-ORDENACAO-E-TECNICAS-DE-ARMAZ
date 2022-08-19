1fcb2abe3af9b1e5f7bebd3e2e08c23505a6f4b5
rafaela nascimento carvalho <rafaelacarvalho.14@hotmail.com>
2022-08-18 22:10:59 -0300

    Primeira aula

---
+++ materiaQuinta/Fibonacci.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace materiaQuinta
+{
+    public class Fibonacci
+    {
+        public Fibonacci(int numeroDesejado)
+        {
+            NumeroDesejado = numeroDesejado;
+        }
+
+        public int NumeroDesejado { get; set; }
+        public IList<int> FibonacciMetodo()
+        {
+            var x = NumeroDesejado;
+            IList<int> retorno = new List<int>();
+
+            var num1 = 0;
+            var num2 = 1;
+            var aux = 0;    
+            var i = 0;
+
+            while (i < x)
+            {
+                aux = num1;
+                num1 = num2;
+                num2 = num1 + aux;
+                i++;
+                retorno.Add(num2);
+            }
+
+            return retorno;
+        }
+    }
+}
---
+++ materiaQuinta/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace materiaQuinta
+{
+    internal class Program
+    {
+        //Aula do dia 18.08
+        static void Main(string[] args)
+        {
+            var x = int.Parse(Console.ReadLine());
+            var fibonacci = new Fibonacci(x);
+
+            IList<int> listMenor = new List<int>();
+            IList<int> listMaior = new List<int>();
+            var numeros = fibonacci.FibonacciMetodo();
+
+            foreach (int numero in numeros)
+            {
+                if(numero > x)
+                {
+                    listMaior.Add(numero);
+                }
+                else
+                {
+                    listMenor.Add(numero);
+                }                
+            }
+            Console.WriteLine($"Menor que: {x}");
+            foreach (int numero in listMenor)
+            {
+                Console.WriteLine(numero);
+            }
+            Console.WriteLine("=========================");
+            Console.WriteLine($"Maior que: {x}");
+            foreach (int numero in listMaior)
+            {
+                    Console.WriteLine(numero);
+            }
+           Console.ReadLine();
+        }
+
+
+    }
+}
